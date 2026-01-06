7cff75edbfe079febcb29da48c7a042bb03de5a7
missionaurelia <julia.meier170783@gmail.com>
2026-01-06 14:27:23 +0100

    Update Universe.jsx

--- src/pages/Universe.jsx
+++ src/pages/Universe.jsx
@@ -2,8 +2,8 @@ import { motion } from 'framer-motion';
 
 export default function Universe() {
   const timelineEvents = [
-    { year: '2025', title: '[Event Title]', description: '[Event description]' },
-    { year: '2030s', title: '[Event Title]', description: '[Event description]' },
+    { year: '2025', title: 'Prolog & Turning Point', description: 'First unconscious signs: a longing for something bigger, a glimpse of the future in everyday moments.' },
+    { year: '2030s', title: 'Upheaval & Manifestation', description: 'First social division between augmented and unaugmented humans. Integration of intelligent humanoid robots called “AIM” (AI Manifesto).' },
     { year: '2040s', title: '[Event Title]', description: '[Event description]' },
     { year: '2050s', title: '[Event Title]', description: '[Event description]' },
     { year: '2060s', title: '[Event Title]', description: '[Event description]' },
